cb5ebe5be95688d4aac195aa491baba338cb484e
Gairik Sharma <88035292+GairikSharma@users.noreply.github.com>
2022-10-20 00:35:14 +0530

    Update Object.js

--- Javascript Basic to Intermediate/Object.js
+++ Javascript Basic to Intermediate/Object.js
@@ -11,3 +11,27 @@ console.log(student.age);
 
 console.log(`Your name is ${student.firstName} and
  your status in facebook login is ${student.isLoggedInFacebook}`);
+
+
+const players = {
+    "Messi" : {
+        "country": "Argentina",
+        "Jercy_number": 10,
+        "current_club": "PSG",
+        "league": "Leag1"
+    },
+    "Ronaldo" : {
+        "country": "Portugal",
+        "Jercy_number": 7,
+        "current_club": "MANU",
+        "league": "EPL"
+    },
+    "Neymar" : {
+        "country": "Brazil",
+        "Jercy_number": 10,
+        "current_club": "PSG",
+        "league": "Leag1"
+    },
+}
+
+console.log(players);
